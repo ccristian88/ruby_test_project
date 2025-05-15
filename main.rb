b1a9f7dcb16d476601e80d4f436aaf63fa03b62a
ccristian88 <ccristian88@outlook.com>
2025-05-15 20:54:36 +0200

    area code dictionary

---
+++ main.rb
@@ -0,0 +1,41 @@
+dial_book = {
+  "newyork" => "212",
+  "newbrunswick" => "732",
+  "edison" => "908",
+  "plainsboro" => "609",
+  "sanfrancisco" => "301",
+  "miami" => "305",
+  "paloalto" => "650",
+  "evanston" => "847",
+  "orlando" => "407",
+  "lancaster" => "717"
+}
+
+def get_city_names(somehash)
+  somehash.keys.each do |key|
+    puts "#{key} "
+  end
+end
+
+def get_area_code(somehash, key)
+  return somehash[key]
+end
+
+loop do
+
+  puts "Welcome to Area code dictionary"
+  25.times { print "-" }
+  puts
+  puts "Here are city names you can see"
+  get_city_names(dial_book)
+  puts "Enter the city name which you get the code for"
+  city_name = gets.chomp
+  code = get_area_code(dial_book, city_name)
+  puts "This is the code: #{code}"
+
+
+  puts "Are you continue to find area code? Y/N";
+  answer = gets.chomp
+  break if answer != "y"
+end
+
